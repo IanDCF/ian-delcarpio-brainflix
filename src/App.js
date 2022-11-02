851a254f184f56ae0a635503cf9613427d75b65e
Ian DC <jokerzamba@hotmail.com>
2022-11-02 15:00:40 +0100

    Create Navbar component structure: Responsive for mobile bp

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
+
 function App() {
   return (
-    <></>
+    <>
+      <Navbar />
+    </>
   );
 }
 
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import "./Navbar.scss";
+import SearchBar from "../SearchBar/SearchBar";
+import UploadBtn from "../UploadBtn/UploadBtn";
+import ProfilePic from "../ProfilePic/ProfilePic";
+import Logo from "../../assets/logo/BrainFlix-logo.svg";
+
+const Navbar = () => {
+  return (
+    <header className="nav-container">
+      <nav className="nav">
+        <img className="nav__logo" src={Logo} alt="BrainFlix logo" />
+
+        <div className="nav__div-one">
+          <div className="nav__div-two">
+            <SearchBar />
+            <div className="nav__pp-mobile">
+              <ProfilePic />
+            </div>
+          </div>
+
+          <UploadBtn />
+        </div>
+
+        <div className="nav__pp-tab-desk">
+          <ProfilePic />
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
